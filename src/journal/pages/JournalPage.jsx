db230a2f4d785afa7fe5ef88945747f40a015236
franzcito <gavino_10@hotmail.es>
2023-01-11 21:04:30 -0500

    agregando estilos al view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom";
-import { DataView } from "../view/dataView";
+import { DataView } from "../view/DataView";
 
 export const JournalPage = () => {
   return (
@@ -12,11 +12,11 @@ export const JournalPage = () => {
               <h1 className=" uppercase font-bold p-2 border-b-2 border-slate-400">franz schwartz</h1>
               
               <ul>
-                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i class="fa-regular fa-calendar-check mx-2"></i><p>ENERO</p></a></li>
-                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i class="fa-regular fa-calendar-check mx-2"></i><p>FEBRERO</p></a></li>
-                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i class="fa-regular fa-calendar-check mx-2"></i><p>MARZO</p></a></li>
-                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i class="fa-regular fa-calendar-check mx-2"></i><p>ABRIL</p></a></li>
-                <li className="p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i class="fa-regular fa-calendar-check mx-2"></i><p>MAYO</p></a></li>
+                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i className="fa-regular fa-calendar-check mx-2"></i><p>ENERO</p></a></li>
+                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i className="fa-regular fa-calendar-check mx-2"></i><p>FEBRERO</p></a></li>
+                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i className="fa-regular fa-calendar-check mx-2"></i><p>MARZO</p></a></li>
+                <li className=" p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i className="fa-regular fa-calendar-check mx-2"></i><p>ABRIL</p></a></li>
+                <li className="p-4 pl-6 hover:bg-slate-400 border-b-2 border-slate-400"><a className="flex items-center" href=""><i className="fa-regular fa-calendar-check mx-2"></i><p>MAYO</p></a></li>
               </ul>
             </nav>
           </div>
@@ -27,7 +27,7 @@ export const JournalPage = () => {
                 <h1 className="font-bold text-white">JournalApp</h1>
                 <Link to={'/auth/login'}><i className="fa-solid fa-right-from-bracket mr-2 text-white"></i></Link>
               </div>
-              <div className="container  p-4  h-screen">
+              <div className="container p-4  h-screen">
                 <DataView/>
               </div>
           </div>
--- src/journal/view/DataView.jsx
+++ src/journal/view/DataView.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 export const DataView = () => {
   return (
-    <div className="container bg-purple-900 rounded-lg h-full flex flex-col justify-center items-center ">
-            <div className="">
-                <p className='text-white'>28 DE DICIEMBRE 2022aea</p>
-                <button className='text-white'>Guardarrr</button>
+    <div className="container  rounded-lg h-full flex flex-col justify-start items-center ">
+            <div className=" flex justify-between  w-full">
+                <p className=' ml-4 text-xl'>28 DE DICIEMBRE 2022</p>
+                <button className=' mr-4'> <i>a</i> GUARDAR</button>
+            </div>
+            <div className="container flex flex-col gap-4 p-2">
+              <input className='w-11/12 p-2 rounded-lg border-2 border-purple-700' type="text" placeholder='ESCRIBA SU TAREA 1 ' />
+              <input className='w-11/12 p-2 rounded-lg border-2 border-purple-700' type="text" placeholder='ESCRIBA SU TAREA 2' />
             </div>
     </div>
   )
